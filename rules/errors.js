f61efe5d2c6a04c62f1210d986f8292ce7e5cb04
Jochem <info@jochemkeller.nl>
2021-10-29 15:05:13 +0200

    initial commit
    Signed-off-by: Jochem <info@jochemkeller.nl>

---
+++ rules/errors.js
@@ -0,0 +1,9 @@
+/* eslint-disable unicorn/prefer-module */
+
+module.exports = {
+    rules: {
+        'no-await-in-loop': 'error',
+        'no-console': 'error',
+        'no-template-curly-in-string': 'error',
+    },
+};
---
+++ rules/vue.js
@@ -0,0 +1,27 @@
+/* eslint-disable unicorn/prefer-module */
+
+module.exports = {
+    rules: {
+        'vuejs-accessibility/media-has-caption': 'off',
+        'vue/block-tag-newline': [
+            'error',
+            {
+                multiline: 'always',
+                singleline: 'always',
+            },
+        ],
+        'vue/component-name-in-template-casing': [
+            'error',
+            'PascalCase',
+            {
+                registeredComponentsOnly: false,
+            },
+        ],
+        'vue/html-indent': ['error', 4],
+        'vue/no-empty-component-block': 'error',
+        'vue/no-template-target-blank': 'error',
+        'vue/padding-line-between-blocks': ['error', 'always'],
+        'vue/singleline-html-element-content-newline': 'off',
+        'vue/v-on-function-call': 'error',
+    },
+};
