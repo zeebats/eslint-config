ab55e0d3b78ed02132b8e83065f26fb9432f6bfa
Daan Vos de Wael <daan@vosdewael.com>
2022-05-29 11:02:07 +0200

    feat: remove `jsdoc` plugin + config

--- rules/jsdoc.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable unicorn/prefer-module */
-
-module.exports = {
-    rules: {
-        'jsdoc/check-indentation': 'warn',
-        'jsdoc/check-syntax': 'warn',
-        'jsdoc/match-description': 'warn',
-    },
-};
