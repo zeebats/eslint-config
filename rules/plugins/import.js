9b3417fe628d8e0578ce0a86dd3bfa7e8f6e376a
Jochem <info@jochemkeller.nl>
2024-03-31 10:25:36 +0200

    feat: qol improvements typescript / imports

--- rules/plugins/import.js
+++ rules/plugins/import.js
@@ -1,7 +1,7 @@
 /* eslint-disable unicorn/prefer-module */
 
 module.exports = {
-	'import/first': 'error',
+	'import/first': 'off',
 	'import/newline-after-import': 'error',
 	'import/order': 'off',
 };
